050d79671e2bab4055bc9bda0b2e10fdbcbbfbda
Rasul <94111424+RASWQQW@users.noreply.github.com>
2023-03-20 10:36:35 +0600

    lasbig

--- Clouder/Obj/CondCode.py
+++
@@ -1,7 +0,0 @@
-from typing import Any
-
-
-class CondCode(object):
-    good: Any | None = "200"
-    bad : Any | None = "400"
-    unbind : Any | None = "404"
--- Clouder/osfiles/conf.py
+++
@@ -1,3 +0,0 @@
-api_id = 16147296
-api_hash = "b37c46483b8f55c3a0221e9c3b859825"
-botApi = "6059490532:AAHk7sYVhcRhBg7Opfn3ieae6_wo7hfWtNU"
